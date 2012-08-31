2aefd008eca889f730af0794f7da09fc612a22bc
Chao Zhang <zhangchao6865@gmail.com>
2012-08-31 23:03:12 +0800

    fix grayify

--- ascii_art_painter.py
+++ ascii_art_painter.py
@@ -2,6 +2,8 @@ import Image, ImageFont, ImageDraw
 import tkFileDialog
 import sys
 import itertools
+import math
+import numpy
 
 src = tkFileDialog.askopenfilename()
 
@@ -19,8 +21,8 @@ print ranks
 
 font = ImageFont.truetype('DejaVuSansMono.ttf', 32)
 
-X_STEP = 4
-Y_STEP = 8
+X_STEP = 1
+Y_STEP = 2
 W_FONT = 20
 H_FONT = 40
 
@@ -32,15 +34,16 @@ try:
 	if src_y % Y_STEP > 0: src_y -= src_y % Y_STEP
 	dest = Image.new('RGB', (src_x * W_FONT / X_STEP, src_y * H_FONT / Y_STEP), (255, 255, 255))
 	dest_d = ImageDraw.Draw(dest)
+	np_pix = numpy.array(srx_pix)
 	for x in range(0, src_x, X_STEP):
 		for y in range(0, src_y, Y_STEP):
 			rgb = [0.0, 0.0, 0.0]
 			for r in range(0, 3):
-				for ii in range(0, X_STEP):
-					for jj in range(0, Y_STEP):
-						rgb[r] += src_pix[+ ii, y + jj][r];
-			rgb = map(lambda a: a / X_STEP / Y_STEP, rgb);
-			gray = (rgb[0] * 0.3 + rgb[1] * 0.59 + rgb[2] * 0.11) / 2
+				for ii in range(x, x + X_STEP):
+					for jj in range(y, y + Y_STEP):
+						rgb[r] += src_pix[ii, jj][r]
+			rgb = map(lambda a: a / X_STEP / Y_STEP, rgb)
+			gray = math.pow((math.pow(rgb[0], 2.2) * 0.2973 + math.pow(rgb[1], 2.2) * 0.6274 + math.pow(rgb[2], 2.2) * 0.0753), 1 / 2.2)
 			dst_x = x * W_FONT / X_STEP
 			dst_y = y * H_FONT / Y_STEP
 			rank = int(gray / 256 * len(ranks))
